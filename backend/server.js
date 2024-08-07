6dff4433abfe9cc32cfae78e5b2d151445a4ed23
Poojan <poojangoyani@gmail.com>
2024-08-08 03:58:19 +0530

    removed cors as there is no use

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import path from "path";
 
 import UserRouter from "./routes/user.routes.js";
@@ -11,12 +10,9 @@ const PORT = process.env.PORT || 3001;
 const __dirname = path.resolve();
 dotenv.config();
 const app = express();
-app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.use(cors());
 
 app.use("/api/users", UserRouter);
 
