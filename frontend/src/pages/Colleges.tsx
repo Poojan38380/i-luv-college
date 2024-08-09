44d47ce3e0182c30a9375d714c8e66e69c16ac9c
Poojan <poojangoyani@gmail.com>
2024-08-09 15:12:24 +0530

    skeletons added when the site is loading

--- frontend/src/pages/Colleges.tsx
+++ frontend/src/pages/Colleges.tsx
@@ -6,10 +6,6 @@ import CollegeCard from "@/components/Colleges/CollegeCard";
 const Colleges = () => {
   const { loading, colleges } = UseGetAllColleges();
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <div className="min-h-screen px-10 mq725:px-5 py-24 mq725:pb-24">
       <div>
@@ -18,15 +14,24 @@ const Colleges = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 mt-10 lg:grid-cols-3 gap-8">
         <AddNew />
-        {colleges.map((college) => (
-          <CollegeCard
-            key={college.id}
-            id={college.id}
-            name={college.name}
-            description={college.description}
-            imageURL={college.images[0]?.imageURL}
-          />
-        ))}
+        {loading
+          ? Array.from({ length: 5 }).map((_, index) => (
+              <div key={index} className="flex  flex-col gap-4">
+                <div className="skeleton h-48 w-full"></div>
+                <div className="skeleton h-4 w-28"></div>
+                <div className="skeleton h-4 w-full"></div>
+                <div className="skeleton h-4 w-full"></div>
+              </div>
+            ))
+          : colleges.map((college) => (
+              <CollegeCard
+                key={college.id}
+                id={college.id}
+                name={college.name}
+                description={college.description}
+                imageURL={college.images[0]?.imageURL}
+              />
+            ))}
       </div>
     </div>
   );
