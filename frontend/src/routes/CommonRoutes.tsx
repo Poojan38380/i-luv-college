73b858ed4b2eaef29193068b396651ba5d64f4b8
Poojan <poojangoyani@gmail.com>
2024-08-11 23:30:47 +0530

    404 page added

--- frontend/src/routes/CommonRoutes.tsx
+++ frontend/src/routes/CommonRoutes.tsx
@@ -9,18 +9,22 @@ import Colleges from "@/pages/Colleges";
 import AddNewCollegePortal from "@/pages/AddNewCollegePortal";
 import CollegePage from "@/pages/CollegePage";
 import CommentPage from "@/pages/CommentPage";
+import NotFound from "@/pages/NotFound";
 
 const CommonRoutes = () => {
   const { authUser } = useAuthContext();
   return (
     <Routes>
-      <Route path="" element={<CommonLayout />}>
+      <Route path="/" element={<CommonLayout />}>
         <Route index element={<Home />} />
         <Route path="/colleges" element={<Colleges />} />
         <Route path="/colleges/page/:collegeId" element={<CollegePage />} />
         <Route path="/colleges/images/:collegeId" element={<CollegePage />} />
         <Route path="/post/comments/:postId" element={<CommentPage />} />
         <Route path="/colleges/add" element={<AddNewCollegePortal />} />
+        {/* other routes */}
+        <Route path="*" element={<NotFound />} />{" "}
+        {/* Catch-all route for 404 */}
       </Route>
       // Routes with no footer
       <Route path="/auth" element={<NoFooterLayout />}>
