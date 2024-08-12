43f4d3d1cb1ec9e7ecab7d983d74769d8b7edaa3
Poojan <poojangoyani@gmail.com>
2024-08-13 00:56:32 +0530

    add sitemaps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,15 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
     react(),
+    sitemap({
+      hostname: "https://i-luv-college.vercel.app", // Replace with your domain
+      outDir: "dist", // Output directory where the sitemap will be generated
+    }),
     sentryVitePlugin({
       org: "poojan-goyani",
       project: "javascript-react",
